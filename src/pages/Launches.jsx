c909424a25fbded045f9c2855bab09f255acca6a
Haeytham Almalak <68405726+haeythamM@users.noreply.github.com>
2025-03-07 15:24:07 -0500

    Second Page Done

--- src/pages/Launches.jsx
+++ src/pages/Launches.jsx
@@ -1,21 +1,35 @@
-import React from "react";
-import LaunchesList from "../components/LaunchesList";
+import React, { useEffect, useState } from 'react';
 import "./Launches.css";
 
 const Launches = () => {
-return (
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.spacexdata.com/v4/launches')
+      .then(response => response.json())
+      .then(data => setLaunches(data));
+  }, []);
+
+  return (
     <div className="launches">
-        <div className="container mt-4">
-            <div className="p-5 bg-secondary text-white rounded">
-                <h1>Launches Page</h1>
-                <p className="lead">
-                    Here you can find details about the latest launches.
-                </p>
-            </div>
-            <LaunchesList />
+      <div className="container mt-4">
+        <div className="p-5 bg-secondary text-white rounded">
+          <h1>Launches Page</h1>
+          <p className="lead">
+            Here you can find details about the latest launches.
+          </p>
         </div>
+        <ul>
+          {launches.map(launch => (
+            <li key={launch.id}>
+              <h2>{launch.name}</h2>
+              <p>{launch.date_utc}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-);
+  );
 };
 
 export default Launches;
