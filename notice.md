4f3db834a649be89604b137e4d8145e1f70f7006
Haeytham Almalak <68405726+haeythamM@users.noreply.github.com>
2025-03-07 20:13:37 -0500

    Notice

---
+++ notice.md
@@ -0,0 +1,10 @@
+**Important Notice**
+
+In this application, some old information from 2022 is mistakenly displayed as "Upcoming" in the future. As a result, I have marked those entries as **Unknown**.
+
+I sincerely apologize for any confusion, especially regarding successful launches that might appear as failed. This issue is due to the way information is being retrieved by the application.
+
+Thank you for your understanding.
+
+**Regards,**<br>
+Haeytham Almalak
--- src/components/LaunchesList.js
+++ src/components/LaunchesList.js
@@ -1,20 +1,20 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
 
-const Launches = () => {
-  const [Launches, setLaunches] = useState([]);
-  useEffect(() => {
-    console.log("Launches component mounted");
-    axios
-      .get("https://api.spacexdata.com/v4/launches/past")
-      .then((Response) => {
-        console.log("Response");
-      });
-  });
-  return (
-    <div>
-      <p>Launches Page</p>
-    </div>
-  );
-};
-export default Launches;
+// const Launches = () => {
+//   const [Launches, setLaunches] = useState([]);
+//   useEffect(() => {
+//     console.log("Launches component mounted");
+//     axios
+//       .get("https://api.spacexdata.com/v4/launches/past")
+//       .then((Response) => {
+//         console.log("Response");
+//       });
+//   });
+//   return (
+//     <div>
+//       <p>Launches Page</p>
+//     </div>
+//   );
+// };
+// export default Launches;
